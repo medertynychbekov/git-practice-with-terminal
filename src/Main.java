896d4adc62ef4524c3d6cca361a917fc3585de96
meder <meder.tynychbekov@gmail.com>
2024-03-18 20:27:10 +0300

    changed some block

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
+        System.out.println("write anyone number: ");
+        int number = scanner.nextInt();
 
-        System.out.println("Your age: " + age);
+        if (number % 2 ==0){
+            System.out.println("Бул жуп сан!");
+        }else {
+            System.out.println("Бул так сан!");
+        }
 
     }
 }
