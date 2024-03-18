63a288cf33fe80cfc301eca47421d141a117ce81
meder <meder.tynychbekov@gmail.com>
2024-03-18 20:19:37 +0300

    I created the new project and add some codes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        System.out.println("Your age: " + age);
+
+    }
+}
